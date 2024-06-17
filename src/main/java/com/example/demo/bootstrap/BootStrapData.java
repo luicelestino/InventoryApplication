c678f02ab3cbeb19fa69b47ffee4a203b1fdbb0a
Luigi Celestino <lcele14@wgu.edu>
2024-06-16 19:33:39 -0700

    Added an import statement in line 4 to import InhousePart from domain Added an import statement in line 8 to import InhousePartRepository from repositories Added an if statement that would populate data with parts and products if repository was empty in lines 64 and 117 Added parts "RTX 4080", "i7 13700K", "B650 GAMING PLUS WIFI ATX AM5", "Corsair", & "RTX 4080 Compatible Case" in lines 65-102 and saved to repository Added products "Base Edition", "Gold Edition", "Platinum Edition", "Diamond Edition", & "Pro Edition" in lines 118-122 and saved to repository

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,11 @@
 package com.example.demo.bootstrap;
 
+
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -29,11 +32,13 @@ public class BootStrapData implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     private final OutsourcedPartRepository outsourcedPartRepository;
+    private final InhousePartRepository inhousePartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository = inhousePartRepository;
     }
 
     @Override
@@ -55,6 +60,48 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println(thePart.getCompanyName());
         */
+
+        if(partRepository.count() == 0) {
+            OutsourcedPart graphicsCard = new OutsourcedPart();
+            graphicsCard.setCompanyName("NVIDIA");
+            graphicsCard.setName("RTX 4080");
+            graphicsCard.setInv(4);
+            graphicsCard.setPrice(1099.99);
+            graphicsCard.setId(121);
+            outsourcedPartRepository.save(graphicsCard);
+
+            OutsourcedPart cpu = new OutsourcedPart();
+            cpu.setCompanyName("Intel");
+            cpu.setName("i7 13700K");
+            cpu.setInv(11);
+            cpu.setPrice(409.99);
+            cpu.setId(122);
+            outsourcedPartRepository.save(cpu);
+
+            OutsourcedPart mobo = new OutsourcedPart();
+            mobo.setCompanyName("MSI");
+            mobo.setName("B650 GAMING PLUS WIFI ATX AM5");
+            mobo.setInv(8);
+            mobo.setPrice(169.99);
+            mobo.setId(123);
+            outsourcedPartRepository.save(mobo);
+
+            OutsourcedPart psu = new OutsourcedPart();
+            psu.setCompanyName("Corsair");
+            psu.setName("RM850e");
+            psu.setInv(10);
+            psu.setPrice(129.99);
+            psu.setId(124);
+            outsourcedPartRepository.save(psu);
+
+            InhousePart tower = new InhousePart();
+            tower.setPartId(125);
+            tower.setName("RTX 4080 Compatible Case");
+            tower.setInv(5);
+            tower.setPrice(134.99);
+            inhousePartRepository.save(tower);
+        }
+
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
@@ -66,6 +113,20 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(bicycle);
         productRepository.save(unicycle);
         */
+
+        if(productRepository.count() == 0) {
+            Product base = new Product("Base Edition", 749.99, 13);
+            Product gold = new Product("Gold Edition", 849.99, 12);
+            Product plat = new Product("Platinum Edition", 949.99, 7);
+            Product diam = new Product("Diamond Edition", 1399.99, 6);
+            Product pro = new Product("Pro Edition", 2099.99, 4);
+
+            productRepository.save(base);
+            productRepository.save(gold);
+            productRepository.save(plat);
+            productRepository.save(diam);
+            productRepository.save(pro);
+        }
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
